78d2d3a947fa42b701f21908c9075a4ccb82eb3c
Мария Веретенникова <veret.maria@gmail.com>
2022-03-02 23:15:06 +0300

    Homework_2 ver_1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 # Главная страница
-def index(request):    
-    return HttpResponse('Главная страница')
+def index(request):
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, template, context)
 
 
+# Страницы с постами
 def group_posts(request, slug):
-    return HttpResponse(f'Здесь будет пост {slug}, если тебе не надоест программировать.')
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {group}'
+    context = {
+        'group': group,
+        'posts': posts,
+        'title': title,
+    }
+    return render(request, template, context)
